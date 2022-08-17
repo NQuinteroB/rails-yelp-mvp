d9225f5d3acada7ff36fcd2b451e56c5db67eb94
Natalia Quintero <nataliaqb.de@gmail.com>
2022-08-17 09:58:04 -0500

    faker seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+# puts "Creating restaurants..."
+
+
+# dishoom = [{ name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'chinese' },
+# { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' },
+# { name: 'Pizza west', address: 'address 1', category: 'japanese' },
+# { name: 'Pizza north', address: 'address 2', category: 'belgian' },
+# { name: 'Pizza south', address: 'address 3', category: 'chinese' }]
+
+# dishoom.each do |attributes|
+#   Restaurant.create!(attributes)
+# end
+
+require 'faker'
+categories = %w[chinese italian japanese french belgian]
+
+5.times do |number|
+  Restaurant.create({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    category: categories.sample,
+    phone_number: Faker::PhoneNumber.cell_phone,
+  })
+puts number
+  end
