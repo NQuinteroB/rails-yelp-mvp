7b7a84865e198fc57cc583836b555c42e7aa15fb
Natalia Quintero <nataliaqb.de@gmail.com>
2023-05-22 11:41:17 +0200

    restaurants controller added, crud actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,6 @@
 # Restaurant.destroy_all
 
 # puts "Creating restaurants..."
-
-
 # dishoom = [{ name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'chinese' },
 # { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' },
 # { name: 'Pizza west', address: 'address 1', category: 'japanese' },
@@ -27,10 +25,11 @@ categories = %w[chinese italian japanese french belgian]
 
 5.times do |number|
   Restaurant.create({
+
     name: Faker::Restaurant.name,
     address: Faker::Address.street_name,
     category: categories.sample,
     phone_number: Faker::PhoneNumber.cell_phone,
   })
-puts number
-  end
+  puts number
+end
